81b0b155c5fa3bffcbf5e8bcd71195d11e1c9ed0
P6g9YHK6 <17877371+P6g9YHK6@users.noreply.github.com>
2026-06-12 09:46:57 +0200

    Add .test-tmp to gitignore, remove tracked test artifacts

--- .test-tmp/scm-test/scripts/existing.ps1
+++
@@ -1,6 +0,0 @@
-echo hi
-
-# --- TRMM METADATA BEGIN ---
-# name: existing
-# code_hash: 91f37295422d95c7e13ad39bd7eaba6fe2a7fead6a08502e534c82f5e9ab6b6b
-# --- TRMM METADATA END ---
--- .test-tmp/scm-test/snippets/test_snippet.ps1
+++
@@ -1,4 +0,0 @@
-# --- TRMM METADATA BEGIN ---
-# name: test_snippet
-# code_hash: abc
-# --- TRMM METADATA END ---
